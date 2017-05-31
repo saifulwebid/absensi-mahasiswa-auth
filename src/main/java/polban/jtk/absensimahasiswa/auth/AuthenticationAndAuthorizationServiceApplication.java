7f4d399f0320d9c73f6f7de8b7a6874ec70a868a
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-05-31 08:06:25 +0700

    Create project in start.spring.io

---
+++ src/main/java/polban/jtk/absensimahasiswa/auth/AuthenticationAndAuthorizationServiceApplication.java
@@ -0,0 +1,12 @@
+package polban.jtk.absensimahasiswa.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndAuthorizationServiceApplication.class, args);
+	}
+}
